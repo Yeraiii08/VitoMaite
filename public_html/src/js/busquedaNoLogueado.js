585300a4965446ea6b043dba0f7830c651308a9e
Yeraiii08 <yeraiigarcia3@gmail.com>
2024-11-24 20:11:31 +0100

    Resultado busqueda de no logueado

---
+++ public_html/src/js/busquedaNoLogueado.js
@@ -0,0 +1,22 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+const btn = document.getElementById("boton-busquedaNoLogueado");
+
+btn.addEventListener("click", function(event){
+    const sexo = document.getElementById("sexo").value;
+    const edadMin = document.getElementById("edad-min").value;
+    const edadMax = document.getElementById("edad-max").value;
+    const ciudad = document.getElementById("ciudad").value;
+    
+    console.log(sexo);
+    console.log(edadMin);
+    console.log(edadMax);
+    console.log(ciudad);
+    
+    if(sexo === "" || edadMin === "" || edadMax === "" || ciudad === ""){
+        alert("algunos campos estan si rellenar");
+    }
+});
